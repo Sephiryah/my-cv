aeedc8d0dbaf3c496b7ce97b837d7b7e02784de0
Sephirah <nicolas.bussiere@outlook.fr>
2023-02-01 11:58:23 +0100

    Add Experience component

---
+++ src/component/Experience.tsx
@@ -0,0 +1,16 @@
+const Experience = () => {
+    return (
+        <div>
+            <h3>Experience Professionel</h3>
+            <ul>
+                <li>
+                    <p><strong>Bénévole</strong></p>
+                    <p>De Mars 2021 à Avril 2021</p>
+                    <p>Je devais nettoyer les loges des animaux, et prendre soin de ces derniers.</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Experience;
